d9643132fa4d97ddc2cbd28d4bcc322ccb02dd4a
Josh Zeng <142065053+zengjosh@users.noreply.github.com>
2024-05-23 23:53:57 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,5 @@
-// Get the email element and the copy button (image)
-const emailElement = document.getElementById('email');
-const copyButton = document.getElementById('copyButton');
-
-// Add a click event listener to the copy button (image)
-copyButton.addEventListener('click', () => {
-  // Create a temporary input element
-  const tempInput = document.createElement('input');
-  
-  // Set the value of the input element to the email address
-  tempInput.value = emailElement.textContent;
-  
-  // Append the input element to the document
-  document.body.appendChild(tempInput);
-  
-  // Select the text in the input element
-  tempInput.select();
-  
-  // Copy the selected text to the clipboard
-  document.execCommand('copy');
-  
-  // Remove the temporary input element
-  document.body.removeChild(tempInput);
-  
-  // Provide feedback to the user
-  // (You can customize this based on the appearance of your image)
-
-});
-
-//carosel
+document.addEventListener('DOMContentLoaded', () => {
+  // Initialize the carousel
   showSlide(currentIndex);
 });
 
