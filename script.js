5957dce750d69f260b0c1e952491e6369a189487
Josh Zeng <142065053+zengjosh@users.noreply.github.com>
2024-05-23 23:45:17 -0700

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,34 @@ copyButton.addEventListener('click', () => {
   // (You can customize this based on the appearance of your image)
 
 });
+
+//carosel
+let currentIndex = 0;
+
+function showSlide(index) {
+  const slides = document.querySelectorAll('.custom-carousel-item');
+  const totalSlides = slides.length;
+
+  if (index >= totalSlides) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = totalSlides - 1;
+  } else {
+    currentIndex = index;
+  }
+
+  const carouselInner = document.querySelector('.custom-carousel-inner');
+  carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  showSlide(currentIndex);
+});
