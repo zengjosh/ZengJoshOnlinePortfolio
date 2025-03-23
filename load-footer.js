c38015a4dd0780c21beee2131268cf776fe5161f
zengjosh <zeng080407@gmail.com>
2025-03-23 16:34:37 -0700

    better footer code

---
+++ load-footer.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('footer.html')
+      .then(response => response.text())
+      .then(data => {
+        document.getElementById('footer').innerHTML = data;
+      })
+      .catch(error => console.error('Error loading footer:', error));
+  });
+  
